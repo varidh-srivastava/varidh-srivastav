704cbbef41153ce667bf5e890e541d9a8e4152e7
varidh-srivastava <91587081+varidh-srivastava@users.noreply.github.com>
2021-10-02 12:23:38 +0530

    day-1 java assignment

---
+++ day-1 java/example.java
@@ -0,0 +1,5 @@
+public class example {
+    public static void main(String[] args){
+        System.out.println("My name is Varidh Srivastav \n I am studing in Acharaya institute of technology \n Persuing BCA 2nd degree \n ");
+    }
+}
